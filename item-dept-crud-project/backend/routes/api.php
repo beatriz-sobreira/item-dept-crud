7992f7ce18c3c8037b02c48e4dbecf2968dec121
beatriz-sobreira <beatrizrsobreira@gmail.com>
2025-08-05 16:11:46 -0300

    Add a test route to web.php for basic functionality check

--- item-dept-crud-project/backend/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\DepartmentController;
-use App\Http\Controllers\ItemController;
-
-Route::apiResource('departments', DepartmentController::class);
-Route::apiResource('items', ItemController::class);
