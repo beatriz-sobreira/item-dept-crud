9c0be8566519918786b037a8b9396da4bdeec0e5
beatriz-sobreira <beatrizrsobreira@gmail.com>
2025-08-05 16:27:56 -0300

    Fix route name from '/teste' to '/test' and ensure Department and Item controllers are properly referenced in web.php

--- item-dept-crud-project/backend/routes/web.php
+++ item-dept-crud-project/backend/routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\ItemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/teste', function () {
+Route::get('/test', function () {
     return 'Rota funcionando!';
 });
+
+Route::resource('departments', DepartmentController::class);
+Route::resource('items', ItemController::class);
